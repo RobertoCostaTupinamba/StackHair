4974ea0d26d1413dc52cc4741bb5c4614421e975
Roberto Costa Tupinamba <41094007+RobertoCostaTupinamba@users.noreply.github.com>
2022-02-11 23:41:08 -0300

    init app

--- app/prettier.config.js
+++ app/prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "avoid",
+  trailingComma: 'all',
+  arrowParens: 'avoid',
   printWidth: 120,
   tabWidth: 2,
-};
+}
